e78b5881446a7aa677e2de7d2931b198ee0691c9
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2020-10-06 21:06:48 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+import requests 
+from bs4 import BeautifulSoup   
 from win10toast import ToastNotifier 
 # create an object to ToastNotifier class
 
@@ -7,7 +7,8 @@ n = ToastNotifier()
 
 # url get link
 def getdata(url):
-    r=requests.get(url)
+    s=requests.get(url)
+    r=s
     return r.text
 htmldata=getdata("https://weather.com/en-IN/weather/today/l/25.59,85.14?par=google&temp=c/")
 soup = BeautifulSoup(htmldata, 'html.parser')
